f496ec245e9b5e1df371605e0c8cba4837d43b58
Todd Sorden <tsorden@mines.edu>
2013-09-26 08:56:17 -0600

    added a quote to dontBeSoSerious

--- BadNews.java
+++ BadNews.java
@@ -4,6 +4,7 @@ public class BadNews {
 	
 	public void dontBeSoSerious(){
 		System.out.println("Whenever you feel sad, just remember that somewhere there in this world there's an idiot pulling a door that says 'PUSH'");
+		System.out.println("I always wanted to be a procrastinator, I just never got around to it");
 		
 	}
 
