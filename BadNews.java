10c16ecd163e9d789f0d68180f3cb1431e7192e4
Todd Sorden <tsorden@mines.edu>
2013-09-26 08:46:52 -0600

    Initial

---
+++ BadNews.java
@@ -0,0 +1,19 @@
+
+public class BadNews {
+	
+	
+	public void testingYou(){
+		System.out.println("Whenever you feel sad, just remember that somewhere there in this world there's an idiot pulling a door that says 'PUSH'");
+		
+	}
+
+	public static void main(String[] args) {
+
+		BadNews justKidding = new BadNews();
+		
+		justKidding.testingYou();
+		
+
+	}
+
+}
