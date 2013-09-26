9022e1bff93cd9e7715661bdd929d93876ae368d
Todd Sorden <tsorden@mines.edu>
2013-09-26 08:51:54 -0600

    Added someQuotes

--- BadNews.java
+++ BadNews.java
@@ -4,7 +4,12 @@ public class BadNews {
 	
 	public void dontBeSoSerious(){
 		System.out.println("Whenever you feel sad, just remember that somewhere there in this world there's an idiot pulling a door that says 'PUSH'");
+	
 		
+	}
+	
+	public void someQuotes(){
+		System.out.println("Education's purpose is to replace an empty mind with an open one");
 	}
 
 	public static void main(String[] args) {
@@ -12,6 +17,7 @@ public class BadNews {
 		BadNews justKidding = new BadNews();
 		
 		justKidding.dontBeSoSerious();
+		justKidding.someQuotes();
 		
 
 	}
