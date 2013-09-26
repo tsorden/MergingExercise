b178ff4d945944f8bf3e9efbadfdf0866760c088
Todd Sorden <tsorden@mines.edu>
2013-09-26 09:01:48 -0600

    changed some things

--- BadNews.java
+++ BadNews.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+
 
 public class BadNews {
 	
+
 	
 	public void dontBeSoSerious(){
 		System.out.println("Whenever you feel sad, just remember that somewhere there in this world there's an idiot pulling a door that says 'PUSH'");
 		System.out.println("I always wanted to be a procrastinator, I just never got around to it");
 		
 	}
+	
 
 	public static void main(String[] args) {
 
